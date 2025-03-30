aa47105344b6437430b3698b7850d5963434f8a4
vokabud <etecture.dubakov@extaccount.com>
2025-03-30 19:59:28 +0300

    Add minimal api

---
+++ Demeter.Api/Demeter.Api/Features/Devices/DevicesApi.cs
@@ -0,0 +1,18 @@
+﻿namespace Demeter.Api.Features.Devices;
+
+public record Device(int Id, string Name);
+
+public static class DevicesEndpoints
+{
+    public static void MapDevicesEndpoints(this IEndpointRouteBuilder app)
+    {
+        app.MapGet("/devices", GetDevices);
+    }
+
+    static IResult GetDevices() => new Random().Next(0, 2) switch
+    {
+        0 => Results.Ok(new[] { new Device (1, "Device 1" ) }),
+        1 => Results.Ok(new[] { new Device (2, "Device 2" ) }),
+        _ => Results.NotFound()
+    };
+}
---
+++ Demeter.Api/Demeter.Api/Program.cs
@@ -0,0 +1,21 @@
+using Demeter.Api.Features.Devices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapDevicesEndpoints();
+
+app.Run();
+
