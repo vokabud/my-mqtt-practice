b27a0fdaf5a85ba9102769a5f8aa90918dd972c7
vokabud <o.dubakov@gmail.com>
2025-03-31 16:32:20 +0300

    Add configuration

---
+++ demeter/Demeter.Api/Configuration/Configure.App.cs
@@ -0,0 +1,21 @@
+﻿using Demeter.Api.Features.Devices;
+
+namespace Demeter.Api.Configuration;
+
+public static partial class Configure
+{
+    public static WebApplication ConfigureApp(this WebApplicationBuilder builder)
+    {
+        var app = builder
+            .ConfigureSwagger()
+            .Build();
+
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.UseHttpsRedirection();
+
+        app.MapDevicesEndpoints();
+
+        return app;
+    }
+}
--- demeter/Demeter.Api/Program.cs
+++ demeter/Demeter.Api/Program.cs
@@ -1,21 +1,7 @@
-using Demeter.Api.Features.Devices;
+using Demeter.Api.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-
-app.MapDevicesEndpoints();
+var app = builder.ConfigureApp();
 
 app.Run();
-
