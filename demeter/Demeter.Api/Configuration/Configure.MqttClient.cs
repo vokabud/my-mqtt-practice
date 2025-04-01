75473e8b82eba420a816c205105ca6d48d749345
vokabud <o.dubakov@gmail.com>
2025-04-01 19:08:22 +0300

    Add configuration

---
+++ demeter/Demeter.Api/Configuration/Configure.MqttClient.cs
@@ -0,0 +1,15 @@
+﻿using Demeter.Api.Workers;
+
+namespace Demeter.Api.Configuration;
+
+public static partial class Configure
+{
+    public static WebApplicationBuilder ConfigureMqttClient(
+               this WebApplicationBuilder builder)
+{
+        builder.Services.AddHostedService<MqttClientService>();
+
+        return builder;
+    }
+}
+
