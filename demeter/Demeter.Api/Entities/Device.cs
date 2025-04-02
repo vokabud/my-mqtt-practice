8039947b108a80a41aa97d1384e3f8a712407bf1
vokabud <o.dubakov@gmail.com>
2025-04-02 15:30:02 +0300

    Add device entities

---
+++ demeter/Demeter.Api/Entities/Device.cs
@@ -0,0 +1,12 @@
+﻿namespace Demeter.Api.Entities;
+
+public class Device
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Type { get; set; }
+
+    public virtual ICollection<DeviceData> Data { get; set; }
+}
---
+++ demeter/Demeter.Api/Entities/DeviceData.cs
@@ -0,0 +1,12 @@
+﻿namespace Demeter.Api.Entities;
+
+public class DeviceData
+{
+    public Guid Id { get; set; }
+
+    public DateTime Timestamp { get; set; }
+
+    public string Value { get; set; }
+
+    public virtual Device Device { get; set; }
+}
